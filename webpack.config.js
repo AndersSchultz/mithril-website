18c3b824c99665987bc2f74eb29ecadcf98dde13
scytrax <mail@anders-schultz.dk>
2017-07-13 21:07:42 +0300

    comments added to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js',//Can I have a comment here?
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
